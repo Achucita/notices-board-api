3a88214ca5ae484450224dece2c808d1d3fde8d6
Bere nice <160133610+Achucita@users.noreply.github.com>
2025-04-23 17:13:16 -0600

    Update notice.routes.js

--- src/routes/notice.routes.js
+++ src/routes/notice.routes.js
@@ -2,7 +2,9 @@ const Router = require('@koa/router');
 const noticeController = require('../controllers/notice.controller');
 
 module.exports = (router) => {
-  const noticeRouter = new Router({ prefix: '/api/notices' });
+  const noticeRouter = new Router({
+        prefix: '/api/notices'
+    });
 
   noticeRouter.post('/', noticeController.create);
   noticeRouter.get('/', noticeController.findAll);
